54054705cb85a274c54238306d06ec4d434f27ee
vera <veradekalb@gmail.com>
2024-12-20 14:14:12 -0800

    mvp: given a wfmu rss feed output pls formatted text

--- parse.lua
+++ parse.lua
@@ -6,7 +6,7 @@ local items = {}
 local elementName = ""
 
 local characterData = function(parser, str)
-	items[#items][elementName] = str
+	items[#items][elementName] = string.gsub(str, "^WFMU MP3 Archive: ", "")
 end
 
 callbacks = {
@@ -32,4 +32,20 @@ for l in io.lines() do -- iterate lines
 end
 p:parse()              -- finishes the document
 p:close()              -- closes the parser
-require 'pl.pretty'.dump(items)
+
+
+-- use if going awk route
+-- local wfmu_fields = { "description", "guid", "link", "pubDate", "title" }
+-- function record_string(...)
+-- 	return string.format(string.rep("%s|", 4) .. "%s\n", ...)
+-- end
+
+io.write("[playlist]")
+for index = 1, #items do
+	local x = items[index]
+	-- need to figure out how to use the values from wfmu_fields here instead of the hardcode
+	io.write(string.format("File%d=%s\n", index, x.link))
+	io.write(string.format("Title%d=%s\n", index, x.title))
+	io.write("\n\n")
+end
+io.write("NumberOfEntries=" .. #items)
